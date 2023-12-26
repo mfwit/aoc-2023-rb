0802b310206926fd2c2f96442a4862743cc46cb7
Matt Witmer <mattwitmer@me.com>
2023-12-26 17:45:17 -0500

    day 1b started

---
+++ day-1/1b.rb
@@ -0,0 +1,21 @@
+numbers = []
+sum = 0
+input = File.open('input').read.split
+input.each do |line|
+  line_numbers = line.delete("^0-9")
+  line_sum = line_numbers[0] + line_numbers[-1]
+  input_numbers << line_sum
+  sum += line_sum.to_i
+end
+
+puts sum
+
+'one'
+'two'
+'three'
+'four'
+'five'
+'six'
+'seven'
+'eight'
+'nine'
